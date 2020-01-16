4a8d46322e876ac4ae3441672c289e24ae5469ec
jackie1120 <hao.huang@outlook.com>
2020-01-16 11:50:49 +0800

    style: 设计日历主界面

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ElementUI from 'element-ui'
-import './styles.scss'
+import ElementUI from "element-ui";
+import moment from "moment";
+import "normalize.css/normalize.css";
+import "./styles.scss";
 
-Vue.use(ElementUI)
-
+Vue.use(ElementUI);
+// Object.defineProperty(Vue, "$moment", moment);
+Vue.prototype.$moment = moment;
 Vue.config.productionTip = false;
 
 new Vue({
