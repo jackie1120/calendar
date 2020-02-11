586870d853c23e32b80ff36f9fed4ee68c06c4b8
jackie1120 <hao.huang@outlook.com>
2020-02-11 16:20:16 +0800

    feat: 完成前后端工程初始化

--- server/app.js
+++ server/app.js
@@ -9,12 +9,12 @@ app.use(bodyParse.json())
 app.use(morgan('combined'))
 require('./router')(app)
 
-app.listen(port, () => {
-  console.log(`Server has started on port ${port}`)
-  sequelize
-    .sync()
-    .then(() => console.log('Database connection has been established'))
-    .catch(error => {
-      console.log('Unabled to connect to the database:', error)
-    })
-})
+sequelize
+  .sync()
+  .then(() => console.log('Database connection has been established'))
+  .then(() => {
+    app.listen(port, () => console.log(`Server has started on port ${port}`))
+  })
+  .catch(err => {
+    console.log('Error has happened: ', err)
+  })
--- server/router/events.js
+++ server/router/events.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const EventController = require('../controller/event')
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('Events')
 })
+router.post('/', EventController.create)
 
 module.exports = router
